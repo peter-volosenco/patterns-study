86a9267fd8e1892ba6fad7e66e22ce83572ec655
pockks <petervolosenco@gmail.com>
2022-07-09 15:08:09 +0100

    1 file modified, 1 added

---
+++ _file5.js
@@ -0,0 +1,44 @@
+// PUB SUB EXAMPLE
+
+// Import stylesheets
+import './style.css';
+import page from './home.html';
+import jQuery from 'jquery';
+
+let appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Specification Pattern</h1>`;
+
+window.jQuery = jQuery;
+window.$ = jQuery;
+
+(function ($) {
+  let o = $({});
+
+  $.subscribe = function () {
+    o.on.apply(o, arguments);
+  };
+
+  $.unsubscribe = function () {
+    o.off.apply(o, arguments);
+  };
+
+  $.publish = function () {
+    o.trigger.apply(o, arguments);
+  };
+})(jQuery);
+
+//subscribe
+function loadPage() {
+  $('#stage').html(page);
+
+  setTimeout(() => {
+    let payload = {
+      url: '',
+      name: 'homepage',
+    };
+
+    $.publish('/page/loaded', [payload]);
+  }, 200);
+}
+
+loadPage();
--- index.js
+++ index.js
@@ -1,42 +1,101 @@
-// Import stylesheets
-import './style.css';
-import page from './home.html';
-import jQuery from 'jquery';
+// Specification pattern
 
-let appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Specification Pattern</h1>`;
+let Color = Object.freeze({
+  red: 'red',
+  green: 'green',
+  blue: 'blue',
+});
 
-window.jQuery = jQuery;
-window.$ = jQuery;
+let Size = Object.freeze({
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+});
 
-(function ($) {
-  let o = $({});
-
-  $.subscribe = function () {
-    o.on.apply(o, arguments);
-  };
-
-  $.unsubscribe = function () {
-    o.off.apply(o, arguments);
-  };
-
-  $.publish = function () {
-    o.trigger.apply(o, arguments);
-  };
-})(jQuery);
-
-//subscribe
-function loadPage() {
-  $('#stage').html(page);
-
-  setTimeout(() => {
-    let payload = {
-      url: '',
-      name: 'homepage',
-    };
-
-    $.publish('/page/loaded', [payload]);
-  }, 200);
+class Product {
+  constructor(name, color, size) {
+    this.name = name;
+    this.color = color;
+    this.size = size;
+  }
 }
 
-loadPage();
+//open for extension, closed for modification
+
+class ProductFilter {
+  filterByColor(products, color) {
+    return products.filter((p) => p.color === color);
+  }
+  filterBySize(products, size) {
+    return products.filter((p) => p.size === size);
+  }
+  filterBySizeAndColor(products, size, color) {
+    return products.filter((p) => p.size === size && p.color === color);
+  }
+  //state space explosion
+  // 3 criteria = 7 methods
+}
+
+// specification
+class ColorSpecification {
+  constructor(color) {
+    this.color = color;
+  }
+
+  isSatisfied(item) {
+    return item.color === this.color;
+  }
+}
+
+class SizeSpecification {
+  constructor(size) {
+    this.size = size;
+  }
+
+  isSatisfied(item) {
+    return item.size === this.size;
+  }
+}
+
+class AndSpecification {
+  constructor(...specs) {
+    this.specs = specs;
+  }
+
+  isSatisfied(item) {
+    return this.specs.every((x) => x.isSatisfied(item));
+  }
+}
+
+let apple = new Product('apple', Color.green, Size.small);
+let tree = new Product('tree', Color.green, Size.large);
+let house = new Product('house', Color.blue, Size.large);
+
+let products = [apple, tree, house];
+
+let pf = new ProductFilter();
+console.log(`Green products (old):`);
+for (let p of pf.filterByColor(products, Color.green)) {
+  console.log(` * ${p.name} is green`);
+}
+
+class BetterFilter {
+  filter(items, spec) {
+    return items.filter((x) => spec.isSatisfied(x));
+  }
+}
+
+let bf = new BetterFilter();
+console.log('Green products (new):');
+for (let p of bf.filter(products, new ColorSpecification(Color.green))) {
+  console.log(` * ${p.name} is green`);
+}
+
+console.log('Large and Green products (new):');
+let specs = new AndSpecification(
+  new ColorSpecification(Color.green),
+  new SizeSpecification(Size.large)
+);
+for (let p of bf.filter(products, specs)) {
+  console.log(` * ${p.name} is large and green`);
+}
