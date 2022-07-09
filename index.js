6b834de35b26c2cdf652b119b37c6e9f2866deb8
pockks <petervolosenco@gmail.com>
2022-07-09 13:46:12 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 // Import stylesheets
 import './style.css';
-
-// import { Between } from 'spec-pattern';
-
-// const rating = new Between(1, 5);
-
-// console.log(rating.isSatisfiedBy(3)); // true
-// console.log(rating.isSatisfiedBy(0)); // true
-
-// console.log('test');
-
 import psp from './pubSubPattern2';
+import page from './home.html';
 
 let appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Specification Pattern</h1>`;
 
-console.log(psp);
+window.psp = psp;
 
 //subscribe
-function pageLoaded(args) {
+function loadPage() {
   // psp.unsub('/page/loaded', pageLoaded);
 
   console.log('pageload with args', args);
 }
-
-let handle = psp.sub('/page/loaded', pageLoaded);
-console.log('handle>', handle);
-
-//publish
-let payload = { x: 0 };
-psp.pub('/page/loaded', [payload]);
-
-payload = { x: 1 };
-psp.pub('/page/loaded', [payload]);
-
-//unsubscribe
-psp.unsub(handle, true);
